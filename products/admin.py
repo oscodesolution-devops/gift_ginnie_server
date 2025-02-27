afca5da8a9abed7dec2e10cc9fa61837a3b99876
aashnajoshi <aashnahetal@gmail.com>
2025-02-27 23:11:55 +0530

    Update admin.py

--- products/admin.py
+++ products/admin.py
@@ -1,21 +1,27 @@
 from django.contrib import admin
-
+from django.contrib.admin import register
+from unfold.admin import ModelAdmin
 from .models import Product, ProductCategory, ProductImage, CarouselItem
 
-
-class ProductAdmin(admin.ModelAdmin):
+@register(Product)
+class ProductAdmin(ModelAdmin):
     list_display = ("name", "category", "created_at", "updated_at")
     search_fields = ("name", "description")
     list_filter = ("category", "created_at", "updated_at")
 
-
-class ProductImageAdmin(admin.ModelAdmin):
+@register(ProductImage)
+class ProductImageAdmin(ModelAdmin):
     list_display = ("product", "image", "created_at", "updated_at")
     search_fields = ("product__name", "image")
     list_filter = ("product", "created_at", "updated_at")
 
+@register(ProductCategory)
+class ProductCategoryAdmin(ModelAdmin):
+    list_display = ("name", "description", "image")
+    search_fields = ("name","description")
 
-admin.site.register(Product, ProductAdmin)
-admin.site.register(ProductCategory)
-admin.site.register(ProductImage, ProductImageAdmin)
-admin.site.register(CarouselItem)
+@register(CarouselItem)
+class CarouselItemAdmin(ModelAdmin):
+    list_display = ("title", "description", "image", "created_at", "order", "is_active")
+    search_fields = ("title", "description", "order")
+    list_filter = ("created_at", "is_active")
