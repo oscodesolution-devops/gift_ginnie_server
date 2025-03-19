017caefe2b31ea2b9e9b71c3af99d6df27868e5b
aashnajoshi <aashnahetal@gmail.com>
2025-03-19 22:14:35 +0530

    Update admin.py

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.contrib import admin
 from django.contrib.admin import register
 from unfold.admin import ModelAdmin
@@ -8,23 +9,62 @@ class ProductImageInline(admin.TabularInline):
     extra = 1
     # todo: Cloudinary image logic to be implemented
 
-class GiftForYouInline(admin.TabularInline):
-    model = GiftForYou
-    extra = 1
-    fields = ("product_category", "product", "display_order")
-    verbose_name = "Gift Item"
-    verbose_name_plural = "Gift Items"
-    readonly_fields = ["product_name"]
+class ProductForm(forms.ModelForm):
+    is_gift_for_you = forms.BooleanField(required=False, label="Add to Gifts for You", initial=False)
+    display_order = forms.IntegerField(required=False, label="Display Order", min_value=0)
 
-    def product_name(self, obj):
-        return obj.product.name
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+    def clean(self):
+        cleaned_data = super().clean()
+        is_gift_for_you = cleaned_data.get('is_gift_for_you')
+        display_order = cleaned_data.get('display_order')
+
+        if is_gift_for_you and display_order is None:
+            raise forms.ValidationError("Display Order must be specified if 'Add to Gifts for You' is selected.")
+        return cleaned_data
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['is_gift_for_you'].widget.attrs.update({'class': 'vCheckboxInput'})
+        self.fields['display_order'].widget.attrs.update({'class': 'vIntegerField'})
 
 @register(Product)
 class ProductAdmin(ModelAdmin):
-    inlines = [ProductImageInline, GiftForYouInline]
-    list_display = ("name", "category", "created_at", "updated_at")
+    form = ProductForm
+    inlines = [ProductImageInline]
+    list_display = ("name", "category", "created_at", "updated_at", "get_is_gift_for_you")
     search_fields = ("name", "description")
     list_filter = ("category", "created_at", "updated_at")
+
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'description', 'category', 'brand', 'original_price', 'selling_price', 'stock')
+        }),
+        ('Gifts for You', {
+            'fields': ('is_gift_for_you', 'display_order'),
+            'description': 'If you want this product to be a gift, select the checkbox and specify the display order.',
+        }),
+    )
+
+    def get_is_gift_for_you(self, obj):
+        return GiftForYou.objects.filter(product=obj).exists()
+    get_is_gift_for_you.boolean = True
+    get_is_gift_for_you.short_description = "Is Gift For You"
+
+    def save_model(self, request, obj, form, change):
+        super().save_model(request, obj, form, change)
+        is_gift_for_you = form.cleaned_data['is_gift_for_you']
+        display_order = form.cleaned_data['display_order']
+        if is_gift_for_you:
+            gift_for_you, created = GiftForYou.objects.get_or_create(product=obj, defaults={'product_category': obj.category, 'display_order': display_order or 0})
+            if not created and gift_for_you.display_order != (display_order or 0):
+                gift_for_you.display_order = display_order or 0
+                gift_for_you.save()
+        else:
+            GiftForYou.objects.filter(product=obj).delete()
 
 @register(ProductImage)
 class ProductImageAdmin(ModelAdmin):
